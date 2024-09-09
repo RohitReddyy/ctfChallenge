909b043d4803f339ad2241b099265699650a4d51
Your Name <you@example.com>
2024-09-08 23:08:09 -0500

    sai

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 
 app.use(cors());
 app.use(express.json());
